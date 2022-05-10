162ee750ad5c439625a8c895801da1504bcae979
bvoo <60059541+bvoo@users.noreply.github.com>
2022-05-09 22:36:59 -0700

    rewrite with multiacc

---
+++ src/MessageHandler.ts
@@ -0,0 +1,75 @@
+import consola from 'consola';
+import open from 'open';
+
+
+async function handleMessage(username: string, message: any) {
+    let res = {
+        req: "",
+        channels: []
+    }
+
+    switch (message.req) {
+        case 'location':
+            consola.info(`${username} | Got location`);
+            break;
+        case 'login':
+            if (message['result'] == 200) {
+                consola.info(`${username} | Logged in`);
+                consola.info(`${username} | Recieved token:`, message['user']['token']);
+
+                let token = message['user']['token'];
+                let uri = `fcade://userstatus/stwlan/${token}`;
+
+                open(uri);
+
+                consola.success(`${username} | Opened ${uri}`);
+
+                res.req = 'getLobbies';
+
+                return res
+            }
+            else {
+                consola.error(`${username} | Login failed:`, JSON.stringify(message));
+            }
+            break;
+        case 'error':
+            consola.error(`${username} | Error`, message.error);
+            break;
+        case 'channels':
+            consola.info(`${username} | Got channels`);
+
+            res = {
+                req: "channels",
+                channels: message.channels
+            }
+
+            return res;
+        case 'join':
+            if (message['result'] == 200) {
+                consola.info(`${username} | Joined channel:`, message['channelname']);
+            }
+            else {
+                if (message['username'] != username) return;
+                consola.error(`${username} | Join failed:`, JSON.stringify(message));
+            }
+        case 'chat':
+            if (!message.username) return;
+            consola.info(`CHAT | ${message.username} | ${message.chat} | ${message.channelname} `);
+        case 'quarkstats':
+        case 'challenge':
+        case 'cancel':
+        case 'stplaying':
+        case 'stnoplaying':
+        case 'staway':
+        case 'stnoaway':
+        case 'leave':
+        case 'stwlan':
+        case 'chaway':
+        case 'updaterank':
+            break;
+        default:
+            consola.error(`${username} | Unknown message`, message);
+    }
+}
+
+export default handleMessage;
---
+++ src/User.ts
@@ -0,0 +1,136 @@
+import WebSocket, { RawData } from 'ws';
+import req from '@aero/http';
+import consola from 'consola';
+import handleMessage from './MessageHandler';
+
+const apiUrl = new URL('https://web.fightcade.com/api');
+const wsUrl = new URL('wss://ggs.fightcade.com/ws/');
+
+class User {
+    constructor(public username: string, private password: string, public idx: number, public requestIdx: number, public priority: number) {
+        this.username = username;
+        this.password = password;
+        this.idx = idx;
+        this.requestIdx = requestIdx;
+        this.priority = priority;
+        let ws: WebSocket | undefined;
+
+        ws = new WebSocket(wsUrl, {
+            headers: { 'User-Agent': 'Fightcade2-WIN32-v2.1.26' },
+            origin: "https://web.fightcade.com",
+            host: "ggs.fightcade.com"
+        });
+
+        this.run(ws);
+    }
+
+    public async run(ws: WebSocket): Promise<void> {
+        return new Promise(async (resolve, reject) => {
+            let location = await this.getLocation();
+
+            consola.info(`${this.username} | Got location`);
+            
+            let timeout = true;
+
+            setTimeout(() => {
+                if (timeout) {
+                    consola.error(`${this.username} | Connection timeout`);
+                    this.run(ws);
+                }
+            }, 15000);
+
+            ws.on('open', () => {
+                timeout = false;
+                consola.success(`${this.username} | Connected to websocket`);
+                this.login(ws, location);
+            });
+
+            ws.on('error', (err: Error) => {
+                consola.error(`${this.username} | Websocket error`, err);
+            });
+
+            ws.on('message', async (message: RawData) => {
+                const data = JSON.parse(message.toString('utf8'));
+                consola.debug(`${this.username} | Got message:`, data['req']);
+                let r = await handleMessage(this.username, data);
+                if (r) {
+                    consola.debug(`${this.username} | Handling message:`, r.req);
+                    if (r.req == 'channels') {
+                        this.joinLobbies(ws, r.channels);
+                    }
+                    if (r.req == 'getLobbies') {
+                        this.getLobbies(ws);
+                    }
+                }
+            });
+
+            ws.on('close', () => {
+                consola.error(`${this.username} | Websocket closed`);
+                resolve();
+            })
+        })
+    }
+
+    public async getLocation() {
+        const res = await req(apiUrl)
+        .method('POST')
+        .body({
+          req: "location"
+        })
+        .json();
+
+        return res.location;
+    }
+
+    public async login(ws: WebSocket, location: string) {
+        let data = {
+            req: "login",
+            username: this.username,
+            userpass: this.password,
+            requestIdx: this.requestIdx,
+            location: location
+        }
+        this.send(ws, data);
+        consola.info(`${this.username} | Logging in`);
+    }
+
+    public async send(ws: WebSocket, data: any) {
+        if (!ws) return;
+        try {
+            consola.debug(`${this.username} | Sending:`, data['req']);
+            ws.send(Buffer.from(JSON.stringify(data)));
+            this.requestIdx += 1;
+            this.idx += 1;
+        }
+        catch (err) {
+            consola.error(`${this.username} | Error sending`, err);
+        }
+    }
+
+    public async getLobbies(ws: WebSocket) {
+        let data = {
+            all: true,
+            req: "channels",
+            requestIdx: this.requestIdx
+        }
+        this.send(ws, data);
+        consola.info(`${this.username} | Getting lobbies`);       
+    }
+
+    public async joinLobbies(ws: WebSocket, channels: any[]) {
+        for (let i = 0; i < 3; i++) {
+            let channel = channels[i + (this.priority - 1) * 3].name;
+            let data = {
+                req: 'join',
+                channelname: channel,
+                idx: this.idx,
+                requestIdx: this.requestIdx,
+                status: 'available'
+            }
+            this.send(ws, data);
+            consola.debug(`${this.username} | Joining lobby ${channels[i].name}`);
+        }
+    }
+}
+
+export default User;
